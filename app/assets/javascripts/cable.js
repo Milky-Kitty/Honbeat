4844a87fb708cf72d8b7b9e7a2225c5981d4185f
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2020-12-01 19:00:49 +0000

    Add a test demonstrating a problem with long submodule paths

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -5,3 +5,10 @@
 //= require action_cable
 //= require_self
 //= require_tree ./channels
+
+(function() {
+  this.App || (this.App = {});
+
+  App.cable = ActionCable.createConsumer();
+
+}).call(this);
