b8e12f301ac82372df97d14fb464101288dae15f
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-01-17 18:30:56 +0000

    Add Cargo.lock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
     def current_user
         return nil unless session[:session_token]
-        @cu
+        @current_user ||= User.find_by(session
