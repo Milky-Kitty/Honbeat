17e3cd7094b45372ac8fb83044253bdea1e5e052
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-01-30 17:09:52 +0000

    Introduce end-of-line normalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,6 @@ class ApplicationController < ActionController::Base
 
     def logout_user!
         current_user.reset_session_token!
-        session[:session
+        session[:session_token] = nil
+    end
+end
