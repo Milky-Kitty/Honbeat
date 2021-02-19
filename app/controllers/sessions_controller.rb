63f35a6b8c17ba48113ae895eb6da6d72337d78a
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-02-19 22:30:08 +0000

    Can work my way through the Kanban

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,4 @@ class SessionsController < ApplicationController
     #login
     def create
         #find if the user exists
-        user = User.find_by_credentials(param
+        user = User.find_by_credentials(params[:user][:email], params[:user
