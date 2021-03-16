3a082e07777530ee84eff72a6b533e477052e841
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-03-16 17:50:37 +0000

    ring-buffer: Fix polling on trace_pipe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,7 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         render :show
-    en
+    end
+    
+    def create
+        @user = User.n
