8f804051179b58d102a8bf5f34532f955500e4e1
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-04-02 21:38:01 +0000

    Update thing.html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,4 +28,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:email, :passwor
+        params.require(:user).permit(:email, :password)
+    end
+
+end
