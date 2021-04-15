e50a47d891cac24f3328cf79d70fdcdb0252ce73
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-04-15 06:50:11 +0000

    Update to version 1.0-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
     validates :email, :session_token, presence: true, uniqueness: true
-    validates :passwor
+    validates :password_digest, presence: { message: '
