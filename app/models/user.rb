3479d232ab19c5d78d5d17051af276907af5f4cd
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-04-16 16:04:21 +0000

    mmc: block: fix race in deferred resume

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     validates :email, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: { message: 'Please enter password' }
 
-    attr_reader :passwor
+    attr_reader :password
+
+    #makes sure we have a toke
