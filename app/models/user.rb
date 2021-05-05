12420d74311bf7fa8e41eeb959e4d20fa28f9ff0
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-05-05 19:44:53 +0000

    added source files

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,7 @@ class User < ApplicationRecord
     end
 
     def is_password?(password)
-        BCrypt::Password.new(self.password_digest).is_password?(pa
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.find_by_creden
