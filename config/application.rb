529236cce5e5a4b89c794f0c20156499b114cfbe
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-08-07 19:21:18 +0000

    hfsplus: fix overflow in sector calculations in hfsplus_submit_bio

--- config/application.rb
+++ config/application.rb
@@ -3,4 +3,5 @@ require_relative 'boot'
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, 
+# you've limited to :test, :development, or :production.
+Bundler.require(*
