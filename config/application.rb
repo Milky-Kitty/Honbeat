2f76826ba7b03251757a08e6b166b7a35b262d8b
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-08-09 16:42:56 +0000

    MIPS: GIC: Prevent array overrun

--- config/application.rb
+++ config/application.rb
@@ -4,4 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*
+Bundler.require(*Rails.groups)
+
+module MusicApp
+  class
