12037e53619bf302ec0ec3fb4208b7cf17bef4f3
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-08-14 20:37:46 +0000

    new test

--- config/application.rb
+++ config/application.rb
@@ -12,4 +12,5 @@ module MusicApp
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files i
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in
