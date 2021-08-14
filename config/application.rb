cf1e9b93cd8a1ec34158d94ba8cd5365cc7d945c
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-08-14 15:49:11 +0000

    This project has been moved to GitLab

--- config/application.rb
+++ config/application.rb
@@ -11,4 +11,5 @@ module MusicApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those speci
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application
