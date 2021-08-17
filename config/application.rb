cb8cc77526bc9073b38a48f0fcfc21b4ed12603d
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-08-17 07:42:36 +0000

    Bump to 0.6.0

--- config/application.rb
+++ config/application.rb
@@ -13,4 +13,5 @@ module MusicApp
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded af
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any
