acd6c93c798d690d2a964acfbf6b881cae959ec3
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-09-06 17:41:24 +0000

    Add to ingest machines.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,2 +1,4 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/appl
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development enviro
