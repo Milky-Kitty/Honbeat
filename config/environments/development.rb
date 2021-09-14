2f12c71eef55b9d3b7c2c67950632ab0a5ba4949
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-09-14 07:02:11 +0000

    temp push

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,4 +10,6 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_reques
+  config.consider_all_requests_local = true
+
+  # Enable/disable cachin
