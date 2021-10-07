545d986e70274ffb74d5d3ba38cbb8df73f8d91a
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-10-07 16:22:34 +0000

    vendor

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,4 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
+    config.action_controller
