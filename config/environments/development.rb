8306cf4bef450ba6d5a8250f02ccf5b14ce3e85b
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-10-17 07:16:06 +0000

    Fix spelling in README

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,4 +24,7 @@ Rails.application.configure do
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store =
+    config.cache_store = :null_store
+  end
+
+  # Store uplo
