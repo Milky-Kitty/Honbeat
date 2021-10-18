6e28d431422999cfb7f08e8c7ee88da2de7b4aa5
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-10-18 11:20:28 +0000

    NFSv4.1: Fix a race in pNFS layoutcommit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,5 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for opti
+  # Store uploaded files on the local file system (see config/storage.yml for options)
+  config.active_storage.serv
