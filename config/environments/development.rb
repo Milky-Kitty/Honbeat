9cd5dc7c640504c741960f9274c67acc635286f4
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-11-15 10:00:38 +0000

    Fix procfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,4 +55,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # Use an evented 
+  # Use an evented file watcher to asynchronously 
