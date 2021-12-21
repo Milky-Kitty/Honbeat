81588001daa831ecfd54e0a063c27b201b4de4be
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-12-21 13:51:55 +0000

    Bump version to 4.1.0

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,4 +18,5 @@ Rails.application.configure do
     'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
-  # Show full error reports and
+  # Show full error reports and disable caching.
+  config.consider_a
