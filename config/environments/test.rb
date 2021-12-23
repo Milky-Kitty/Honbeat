89d3222ef4a4e6b3d592b09045d67500ab320d8f
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-12-23 22:23:30 +0000

    wireless: rt2x00: add new rt2800usb device

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,4 +20,6 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.per
+  config.action_controller.perform_caching = false
+
+  # Rai
