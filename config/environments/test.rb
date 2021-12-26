1aae7cee335e2fbef809a7e5217d0379ca89f129
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-12-26 06:49:32 +0000

    forgot to commit this

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,4 +22,5 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Raise exceptions instead of renderi
+  # Raise exceptions instead of rendering exception templates.
+  config
