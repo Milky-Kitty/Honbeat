7e6c436f541ba0ffee6cefdd1844204ac601a9f7
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2021-12-28 08:44:43 +0000

    Remove debug statements.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,4 +26,4 @@ Rails.application.configure do
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment.
-  config.
+  config.action_controller.allow_fo
