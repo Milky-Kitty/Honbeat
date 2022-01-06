6d5838610e1e81078ff11c10f7b3c8a1656c57cd
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2022-01-06 06:22:18 +0000

    we want to rename this column, and also fix the trigger

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,4 +28,5 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
-  # Store uploaded files on the local file
+  # Store uploaded files on the local file system in a temporary directory
+  
