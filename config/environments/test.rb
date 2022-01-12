822a1c7c3ac8f6d308d7944b2c5b61455bcb2d9c
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2022-01-12 10:08:28 +0000

    massive refactoring

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,3 +30,5 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system in a temporary directory
   config.active_storage.service = :test
+
+  config.action_mailer.perform_cachin
