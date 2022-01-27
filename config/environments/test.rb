8d5240bfabd2cac2cc59ba1a700a5d97a7336459
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2022-01-27 16:35:57 +0000

    lot of changes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
-  config.active_suppo
+  config.active_support.deprecation = :stderr
+
+  # Raises error for
