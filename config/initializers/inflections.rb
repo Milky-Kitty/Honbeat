1dcbebe41ca6c642004d9e42abe5a001ee66535b
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2022-02-26 08:13:07 +0000

    Sending translation for Spanish

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -2,4 +2,4 @@
 
 # Add new inflection rules using the following format. Inflections
 # are locale specific, and you may define rules for as many different
-# locales as you wish. All of 
+# locales as you wish. All of these examples are active by 
