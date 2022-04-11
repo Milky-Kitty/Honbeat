132c4f7ab2549846cf38f07d58e083ea5c949aba
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2022-04-11 15:36:00 +0000

    updated docs

--- config/puma.rb
+++ config/puma.rb
@@ -33,3 +33,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # process behavior so workers use less memory.
 #
 # preload_app!
+
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
