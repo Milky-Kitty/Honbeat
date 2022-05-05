3d0bb67de75bfbdf6915a8626f01281da8652432
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2022-05-05 22:41:21 +0000

    sched: LOAD_FREQ (4*HZ+61) avoids loadavg Moire

--- db/schema.rb
+++ db/schema.rb
@@ -2,3 +2,4 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
+# Note that this schema.rb definitio
