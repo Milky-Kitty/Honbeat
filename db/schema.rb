e323762875fde342c8d8aa9f77086f598aba156c
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2022-05-11 06:08:29 +0000

    #47

--- db/schema.rb
+++ db/schema.rb
@@ -2,4 +2,4 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definitio
+# Note that this schema.rb definition is the authoritative source for 
