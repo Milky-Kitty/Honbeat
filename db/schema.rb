c44e3264dccae11f447909b0230292e2d15507ea
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2022-06-28 18:28:58 +0000

    estilo

--- db/schema.rb
+++ db/schema.rb
@@ -20,4 +20,5 @@ ActiveRecord::Schema.define(version: 2020_03_25_190913) do
     t.text "password_digest", null: false
     t.text "session_token", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null
+    t.datetime "updated_at", null: false
+    t.index ["email"], nam
