6803fa9cf438c0864f8a8505f5922268c878edfb
Milky-Kitty <102315183+Milky-Kitty@users.noreply.github.com>
2022-06-28 18:22:40 +0000

    Piaget

--- db/schema.rb
+++ db/schema.rb
@@ -19,4 +19,5 @@ ActiveRecord::Schema.define(version: 2020_03_25_190913) do
     t.text "email", null: false
     t.text "password_digest", null: false
     t.text "session_token", null: false
-    t.datetime "created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null
